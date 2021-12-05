43a27b54181fc25fd5b9e7388902af6814ea4ec6
Andrea Freeman <afreeman5566@gmail.com>
2021-12-05 11:47:46 -0600

    added controllers and routes

--- example-project/config/routes.rb
+++ example-project/config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+
+
+
+  resources :users, only: [:index, :show]
+  resources :carts, only: [:show]
+  resources :teas, only: [:index]
+
+
+  #test routes
+
   get "/hello", to: "application#hello_world"
 
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
